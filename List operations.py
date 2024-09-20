5a63ee0a824eb7fc39d6d1f108010fb6a0060b31
Junaira <advshakerullah@gmail.com>
2024-09-20 18:05:54 +0600

    first commit

---
+++ List operations.py
@@ -0,0 +1,20 @@
+li = ["Junaira","Tabassum","Kashfia"]
+print(li)
+
+print(      len(li)      )
+
+print(      li[0]      )
+print(      li[1]      )
+print(      li[2]      )
+
+li.append("Nahian")
+li.append("Giga")
+
+print(      li[3]      )
+print(      li[4]      )
+
+print(li)
+
+alist = [100, 200, 300, 400, 500]
+
+print(    alist[::-1]    )
